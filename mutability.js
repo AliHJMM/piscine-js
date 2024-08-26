35126b4f371a779091e4c6b16c65b9e0d8803054
Ali <AliHasanJMM@gmail.com>
2024-08-26 14:04:41 +0300

    Fourth Quest

--- mutability.js
+++ mutability.js
@@ -1,18 +1,6 @@
-const person = {
-  name: "Rick",
-  age: 77,
-  country: "US",
-};
-
 const clone1 = { ...person };
 const clone2 = { ...person };
 const samePerson = person;
 
-
 person.age++;
 person.country = `FR`;
-
-console.log("Person:", person);
-console.log("Clone1:", clone1);
-console.log("Clone2:", clone2);
-console.log("SamePerson:", samePerson);
