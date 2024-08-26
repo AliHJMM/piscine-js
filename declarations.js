e4cd6952673a679bed3e1744581230f7b81aea99
Ali <AliHasanJMM@gmail.com>
2024-08-26 12:57:34 +0300

    Third Quest

---
+++ declarations.js
@@ -0,0 +1,19 @@
+// const escapeStr =;
+const arr = [4,'2']
+const obj = {
+    str: "Ali",
+    num: 20,
+    bool: true,
+    undef: undefined,
+    nested: {
+        arr: [4, undefined, '2'],
+        obj: {
+            str: "Hadi",
+            age: 16,
+            bool:true,
+        }
+    }
+}
+Object.freeze(nested);
+Object.freeze(arr);
+Object.freeze(obj);
