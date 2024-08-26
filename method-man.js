5a18c7f9f172c9a5df373c599151d57e529f41e0
Ali <AliHasanJMM@gmail.com>
2024-08-26 22:54:49 +0300

    Quest 13

--- method-man.js
+++ method-man.js
@@ -2,4 +2,4 @@ const words = (str) => str.split(' ');
 const sentence = (strArr) => strArr.join(' ');
 const yell = (str) => str.toUpperCase();
 const whisper = (str) => str.toLowerCase();
-const capitalize = (str) => (str.toLowerCase()) (str[0].toUpperCase()) ;
+const capitalize = (str) => (str.charAt(0).toUpperCase()) + (str.substring(1).toLowerCase()) ;
