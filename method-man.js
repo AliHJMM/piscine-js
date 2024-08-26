6ddf6c1ae8aca07cab57bf6320efdc0212c284ec
Ali <AliHasanJMM@gmail.com>
2024-08-26 22:52:10 +0300

    Quest 13

---
+++ method-man.js
@@ -0,0 +1,5 @@
+const words = (str) => str.split(' ');
+const sentence = (strArr) => strArr.join(' ');
+const yell = (str) => str.toUpperCase();
+const whisper = (str) => str.toLowerCase();
+const capitalize = (str) => (str.toLowerCase()) (str[0].toUpperCase()) ;
