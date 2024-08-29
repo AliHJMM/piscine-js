8a793429675be6b0fa5e4937736281a39300c95a
alihasan6 <AliHasanJMM@gmail.com>
2024-08-29 20:19:21 +0300

    elementary

--- elementary.js
+++ elementary.js
@@ -1,14 +1,33 @@
 const multiply = (a, b) => {
     let result = 0;
+    let negative = false;
+
+    // Determine if the result should be negative
+    if (b < 0) {
+        b = -b;
+        negative = !negative;
+    }
+    if (a < 0) {
+        a = -a;
+        negative = !negative;
+    }
+
+    // Perform repeated addition
     for (let i = 0; i < b; i++) {
         result += a;
     }
+
+    // If the result should be negative, negate it
+    if (negative) {
+        result = -result;
+    }
+
     return result;
 };
 
 const divide = (a, b) => {
     if (b === 0) {
-        return Infinity; 
+        return Infinity; // Or use another value to represent undefined division
     }
 
     let result = 0;
@@ -18,12 +37,13 @@ const divide = (a, b) => {
         dividend -= b;
         result++;
     }
+
     return result;
 };
 
 const modulo = (a, b) => {
     if (b === 0) {
-        return undefined;
+        return undefined; // Or use another value to represent undefined modulo
     }
 
     let dividend = a;
