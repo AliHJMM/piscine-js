bace83764ba6f0d36ff77cac48d4b112ce29688d
Ali <AliHasanJMM@gmail.com>
2024-09-02 16:50:37 +0300

    Quest

--- rebecca-black.js
+++ rebecca-black.js
@@ -6,3 +6,8 @@ function isWeekend(date) {
     var day = new Date(date).getDay();
     return day === 0 || day === 6;
 }
+
+function isLeapYear(date) {
+    var year = new Date(date).getFullYear();
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
