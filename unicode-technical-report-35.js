6f4bfac3e5e3186690266ce886bb237cd410703a
Ali <AliHasanJMM@gmail.com>
2024-09-02 16:55:46 +0300

    Quest

---
+++ unicode-technical-report-35.js
@@ -0,0 +1,5 @@
+Date.prototype.month = Date.prototype.getMonth;
+Date.prototype.day = Date.prototype.getDay;
+Date.prototype.year = Date.prototype.getFullYear;
+Date.prototype.date = Date.prototype.getDate;
+Date.prototype.hours = Date.prototype.getHours;
