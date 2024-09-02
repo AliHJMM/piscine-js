a057456fbf3265811bf93e3530ba75dbe06e60ae
Ali <AliHasanJMM@gmail.com>
2024-09-02 16:56:21 +0300

    Quest

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -34,3 +34,8 @@ function format(date, f) {
     // Day
     f = f.replace(/dd/g, ("0" + d.date()).slice(-2));
     f = f.replace(/d/g, d.date());
+      // Hour
+      f = f.replace(/HH/g, ("0" + d.hours()).slice(-2));
+      f = f.replace(/H/g, d.hours());
+      f = f.replace(/hh/g, ("0" + (d.hours() % 12 || 12)).slice(-2));
+      f = f.replace(/h/g, d.hours() % 12 || 12);
