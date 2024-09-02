bc5076f4f319852622f939388a539a4708657918
Ali <AliHasanJMM@gmail.com>
2024-09-02 16:45:56 +0300

    Quest

---
+++ date-is.js
@@ -0,0 +1,9 @@
+function isValid(date) {
+    if (new Date(date).toString() === "Invalid Date") {
+        return false;
+    }
+    if (!(date instanceof Date) && typeof date !== "number") {
+        return false;
+    }
+    return true;
+}
