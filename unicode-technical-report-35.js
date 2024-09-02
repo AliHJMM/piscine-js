7ee5d1a536e979a82ae59b672d0af74f594c089a
Ali <AliHasanJMM@gmail.com>
2024-09-02 16:55:55 +0300

    Quest

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -3,3 +3,20 @@ Date.prototype.day = Date.prototype.getDay;
 Date.prototype.year = Date.prototype.getFullYear;
 Date.prototype.date = Date.prototype.getDate;
 Date.prototype.hours = Date.prototype.getHours;
+
+function format(date, f) {
+    const d = new Date(date);
+    const lM = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
