9ef4d287865ae99720f5f5e4665d39ae448e8152
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 20:40:11 +0300

    Quest

--- using-filter.js
+++ using-filter.js
@@ -15,3 +15,14 @@ function filter1DistinctVowel(arr) {
         (item) => new Set(item.toLowerCase().match(/[aeiou]/gi)).size === 1
     );
 }
+
+function multiFilter(arr) {
+    arr = arr.filter((item) => {
+        let capital = item.capital.length >= 8;
+        let name = !/^[aeiou]/i.test(item.name);
+        let tag = /[aeiou]/i.test(item.tag);
+        let region = item.region !== "South";
+        return capital && name && tag && region;
+    });
+    return arr;
+}
