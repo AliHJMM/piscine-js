045047ab3c8c9979711ec85c9b15b64948423a8a
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 20:40:52 +0300

    Quest

--- using-reduce.js
+++ using-reduce.js
@@ -4,3 +4,16 @@ function adder(arr, value) {
         value === undefined ? 0 : value
     );
 }
+
+function sumOrMul(arr, value) {
+    return arr.reduce(
+        (acc, item) => {
+            if (item % 2 === 0) {
+                return acc * item;
+            } else {
+                return acc + item;
+            }
+        },
+        value === undefined ? 0 : value
+    );
+}
