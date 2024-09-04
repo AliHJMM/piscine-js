de6652f9d918e329e22ac9e32a4b65ba4dc812c8
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 20:58:09 +0300

    Quest

--- quantifiers.js
+++ quantifiers.js
@@ -4,3 +4,10 @@ function every(array, test) {
     }
     return true;
 }
+
+function some(array, test) {
+    for (var i = 0; i < array.length; i++) {
+        if (test(array[i])) return true;
+    }
+    return false;
+}
