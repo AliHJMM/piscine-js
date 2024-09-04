d98f5788a43ea8a809dc09b75a37d7b8eb2e7f3a
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 20:39:42 +0300

    Quest

--- using-filter.js
+++ using-filter.js
@@ -1,3 +1,7 @@
 function filterShortStateName(arr) {
     return arr.filter((item) => item.length < 7);
 }
+
+function filterStartVowel(arr) {
+    return arr.filter((item) => /^[aeiou]/i.test(item));
+}
