6bf92b2a403756036255921acc8b8fd2721d0855
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 20:38:38 +0300

    Quest

--- using-map.js
+++ using-map.js
@@ -1,3 +1,14 @@
 function citiesOnly(arr) {
     return arr.map((item) => item.city);
 }
+
+function upperCasingStates(arr) {
+    return arr.map((item) =>
+        item
+            .split(" ")
+            .map((word) => {
+                return word[0].toUpperCase() + word.slice(1);
+            })
+            .join(" ")
+    );
+}
