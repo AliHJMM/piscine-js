e9ba9992c3159a9f200981b2ec26826afda9fa39
alihasan6 <AliHasanjmm@gmail.com>
2024-09-04 21:10:40 +0300

    Quest

--- sweet-curry.js
+++ sweet-curry.js
@@ -1,3 +1,11 @@
 function mult2(x) {
     return (y) => x * y;
 }
+
+function add3(x) {
+    return function (y) {
+        return function (z) {
+            return x + y + z;
+        };
+    };
+}
