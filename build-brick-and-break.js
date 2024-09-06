edc5538a9fa2a24030b8fd966ff525211b25adce
Ali <AliHasanJMM@gmail.com>
2024-09-06 03:58:01 +0300

    Quest

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,17 +1,20 @@
-function build(n) {
-    let body = document.getElementsByTagName("body")[0];
+const build = (n) => {
+    const body = document.getElementsByTagName("body")[0];
     let bricks = 1;
-    let interval = setInterval(() => {
-        let brick = document.createElement("div");
+    const interval = setInterval(() => {
+        const brick = document.createElement("div");
         brick.setAttribute("id", "brick-" + bricks);
-        bricks % 3 === 2 ? (brick.dataset.foundation = true) : null;
+        if (bricks % 3 === 2) {
+            brick.dataset.foundation = true;
+        }
         body.appendChild(brick);
         bricks++;
         if (bricks > n) {
             clearInterval(interval);
         }
     }, 100);
-}
+};
+
 
 function repair(...ids) {
     ids.forEach((id) => {
