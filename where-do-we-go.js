3d58654b0296ec38806d57c18b7cc41db84b54d2
Ali <AliHasanJMM@gmail.com>
2024-09-08 00:41:35 +0300

    Quest

--- where-do-we-go.js
+++ where-do-we-go.js
@@ -7,3 +7,11 @@ document.body.appendChild(location);
 document.addEventListener("DOMContentLoaded", () => {
     selectPlace();
 });
+
+document.addEventListener("scroll", () => {
+    selectPlace();
+    scroll > window.scrollY
+        ? (document.querySelector(".direction").innerHTML = "N")
+        : (document.querySelector(".direction").innerHTML = "S");
+    scroll = window.scrollY;
+});
