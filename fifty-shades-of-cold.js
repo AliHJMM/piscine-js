9579fb6b646a72024714c82d072396fab11c5bd8
Ali <AliHasanJMM@gmail.com>
2024-09-07 19:48:32 +0300

    Quest

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,1 +1,11 @@
 import { colors } from "./fifty-shades-of-cold.data.js";
+
+function generateClasses() {
+    const head = document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    colors.forEach((color) => {
+        style.innerHTML += `.${color} {\n  background: ${color};\n }\n\n`;
+    });
+    console.log(style.innerHTML);
+    head.appendChild(style);
+}
