4d4c3a3e686c1ada440432d6731c47033791fd2c
Ali <AliHasanJMM@gmail.com>
2024-09-08 00:38:21 +0300

    Quest

--- gossip-grid.js
+++ gossip-grid.js
@@ -1,1 +1,27 @@
 import { gossips } from "./gossip-grid.data.js";
+
+function grid() {
+    ranges();
+    let form = document.createElement("form");
+    form.classList.add("gossip");
+    let textarea = document.createElement("textarea");
+    let button = document.createElement("button");
+    button.innerHTML = "Share gossip!";
+    button.type = "submit";
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        let gossip = textarea.value;
+        if (gossip.length > 0) {
+            gossips.unshift(gossip);
+            document.querySelectorAll(".gossip").forEach((card, i) => {
+                if (i > 0) card.remove();
+            });
+            textarea.value = "";
+            renderGossips();
+        }
+    });
+    form.appendChild(textarea);
+    form.appendChild(button);
+    document.body.appendChild(form);
+    renderGossips();
+}
