36e6824cd8e387576d17340af31c257dc616cd70
Ali <AliHasanJMM@gmail.com>
2024-09-08 00:23:41 +0300

    Quest

--- mouse-trap.js
+++ mouse-trap.js
@@ -11,3 +11,14 @@ class Circle {
         this.draw();
         circles.push(this);
     }
+
+    draw() {
+        this.HTML = document.createElement("div");
+        this.HTML.classList.add("circle");
+        this.HTML.style.position = "absolute";
+        this.HTML.style.top = this.y + "px";
+        this.HTML.style.left = this.x + "px";
+        this.HTML.style.background = "white";
+        this.trapped();
+        document.body.appendChild(this.HTML);
+    }
