b5abfe40d73a8ceb6da8bc87acef1f213b512a03
Your Name <you@example.com>
2024-09-10 12:49:35 +0300

    Object

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -16,10 +16,18 @@ const mapKeys = (obj, callback) => {
         }, {});
 }
 
-const reduceKeys = (obj, callback, initialValue) => {
-    let accumulator = initialValue !== undefined ? initialValue : '';
-
-    return Object.keys(obj).reduce((acc, currentKey) => {
-        return callback(acc, currentKey);
-    }, accumulator).replace(/^,\s*/, ''); 
+function reduceKeys(obj, callback, initialValue) {
+    let undef = false;
+    if (initialValue === undefined) {
+        initialValue = "";
+        undef = true;
+    }
+    let res = Object.keys(obj).reduce((acc, curr) => {
+        return callback(acc, curr, initialValue);
+    }, initialValue);
+    if (typeof res !== "number") {
+        if (res.slice(0, 2) === ", ") res = res.slice(2);
+        if (undef && res[0] === ":") res = res.slice(1);
+    }
+    return res;
 }
