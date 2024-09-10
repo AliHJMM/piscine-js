98973394e0ea8f18b73dcd85889d4eb031d04766
Your Name <you@example.com>
2024-09-10 12:38:04 +0300

    Object

--- manipulate-values.js
+++ manipulate-values.js
@@ -16,7 +16,7 @@ const filterValues = (obj, callback) => {
     return result;
   }
 
-  function reduceValues(obj, callback, initialValue) {
+  const reduceValues = (obj, callback, initialValue) => {
     let accumulator = initialValue;
     for (const value of Object.values(obj)) {
       accumulator = callback(accumulator, value);
