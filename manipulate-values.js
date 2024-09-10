4b622236818353679f138434b2d6cf02f259a7d6
Your Name <you@example.com>
2024-09-10 12:36:14 +0300

    Object

---
+++ manipulate-values.js
@@ -0,0 +1,9 @@
+function filterValues(obj, callback) {
+    const result = {};
+    for (const [key, value] of Object.entries(obj)) {
+      if (callback(value)) {
+        result[key] = value;
+      }
+    }
+    return result;
+  }
