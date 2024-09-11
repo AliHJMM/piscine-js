c65d4f5334c0a4cbe13d63b25071896acd67dc07
Ali <AliHasanJMM@gmail.com>
2024-09-11 18:50:31 +0300

    obj

---
+++ deep-copy.js
@@ -0,0 +1,17 @@
+const deepCopy = (input) => {
+    if (Array.isArray(input)) {
+        var output = [];
+        for (var i = 0; i < input.length; i++) {
+            output[i] = deepCopy(input[i]);
+        }
+        return output;
+    } else if (isDefenitelyAnObject(input)) {
+        var output = {};
+        for (var key in input) {
+            output[key] = deepCopy(input[key]);
+        }
+        return output;
+    } else {
+        return input;
+    }
+}
