a34afc2f1056199d7325666401cb2d8ec223b425
Ali <AliHasanJMM@gmail.com>
2024-09-13 13:36:49 +0300

    Aysnc

--- debounce.js
+++ debounce.js
@@ -6,3 +6,27 @@ const debounce=(func, wait)=> {
         timeout = setTimeout(() => func.apply(context, args), wait);
     };
 }
+
+function opDebounce(func, wait, leading = false) {
+    let timeout;
+    let lastCall = 0;
+
+    return function(...args) {
+        const context = this;
+        const now = Date.now();
+
+        if (leading && !lastCall) {
+            func.apply(context, args);
+        }
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            lastCall = 0; // Reset last call timestamp after waiting
+            if (!leading) {
+                func.apply(context, args);
+            }
+        }, wait);
+
+        lastCall = now;
+    };
+}
