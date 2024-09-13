3d22c6f7215dd7a969736358ccb5dfaaf0323ed2
Ali <AliHasanJMM@gmail.com>
2024-09-14 00:10:07 +0300

    aysnc

--- throttle.js
+++ throttle.js
@@ -9,3 +9,37 @@ function throttle(func, wait) {
         }
     };
 }
+
+function opThrottle(func, wait, { leading = true, trailing = true } = {}) {
+    let timeout = null;
+    let lastCall = 0;
+    let lastArgs;
+    let context;
+
+    const invokeFunc = () => {
+        func.apply(context, lastArgs);
+        lastCall = Date.now();
+        timeout = null;
+    };
+
+    return function(...args) {
+        const now = Date.now();
+        context = this;
+        lastArgs = args;
+
+        if (!lastCall && !leading) lastCall = now;
+
+        const remainingTime = wait - (now - lastCall);
+
+        if (remainingTime <= 0 || remainingTime > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            lastCall = now;
+            func.apply(context, args);
+        } else if (trailing && !timeout) {
+            timeout = setTimeout(invokeFunc, remainingTime);
+        }
+    };
+}
