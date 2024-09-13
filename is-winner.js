006a4d8126dd797cf6a862db1b68209105824da0
Ali <AliHasanJMM@gmail.com>
2024-09-14 00:18:48 +0300

    aysnc

--- is-winner.js
+++ is-winner.js
@@ -1,19 +1,23 @@
-async function isWinner(country) {
+const isWinner = async (country) => {
     try {
         country = await db.getWinner(country);
-        if (country === Error('Country Not Found')) {
+        if (country instanceof Error && country.message === 'Country Not Found') {
             return `${country.name} never was a winner`;
         }
+
         if (country.continent !== 'Europe') {
             return `${country.name} is not what we are looking for because of the continent`;
         }
+
         let results = await db.getResults(country.id);
-        if (results === Error('Results Not Found')) {
+        if (results instanceof Error && results.message === 'Results Not Found') {
             return `${country.name} never was a winner`;
         }
+
         if (results.length < 3) {
             return `${country.name} is not what we are looking for because of the number of times it was champion`;
         }
+
         return (
             `${country.name} won the FIFA World Cup in ` +
             results.map((result) => result.year).join(', ') +
@@ -24,5 +28,6 @@ async function isWinner(country) {
         if (e.message === 'Country Not Found') {
             return `${country} never was a winner`;
         }
+        return `An error occurred: ${e.message}`;
     }
-}
+};
